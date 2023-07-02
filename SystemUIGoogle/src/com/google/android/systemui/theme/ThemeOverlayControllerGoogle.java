a07e1c9fd91c9e38c8357840655778da46cd0247
Chris <chrisfilho333@gmail.com>
2023-07-02 00:43:44 +0000

    Revert "SystemUIGoogle: Re-evaluate system theme on UI mode change [2/1]"
    This reverts commit 543e55d04d604c26ec5c605b919a353511d99149.

--- SystemUIGoogle/src/com/google/android/systemui/theme/ThemeOverlayControllerGoogle.java
+++ SystemUIGoogle/src/com/google/android/systemui/theme/ThemeOverlayControllerGoogle.java
@@ -33,7 +33,6 @@ import com.android.systemui.flags.SystemPropertiesHelper;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.policy.ConfigurationController;
-import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.theme.ThemeOverlayApplier;
 import com.android.systemui.theme.ThemeOverlayController;
@@ -48,16 +47,6 @@ public class ThemeOverlayControllerGoogle extends ThemeOverlayController {
     private final Resources resources;
     private final SystemPropertiesHelper systemProperties;
 
-    private final ConfigurationController darkConfigurationController;
-    private final ConfigurationListener darkConfigurationListener =
-            new ConfigurationListener() {
-                @Override
-                public void onUiModeChanged() {
-                    Log.d(TAG, "Re-applying theme on UI change");
-                    reevaluateSystemTheme(true /* forceReload */);
-                }
-            };
-
     @Inject
     public ThemeOverlayControllerGoogle(Context context, BroadcastDispatcher broadcastDispatcher,
                                         @Background Handler bgHandler, @Main Executor mainExecutor,
@@ -71,10 +60,9 @@ public class ThemeOverlayControllerGoogle extends ThemeOverlayController {
         super(context, broadcastDispatcher, bgHandler, mainExecutor, bgExecutor,
                 themeOverlayApplier, secureSettings, wallpaperManager, userManager,
                 deviceProvisionedController, userTracker, dumpManager, featureFlags,
-                resources, wakefulnessLifecycle, configurationController);
+                resources, wakefulnessLifecycle);
         this.systemProperties = systemPropertiesHelper;
         this.resources = resources;
-        darkConfigurationController = configurationController;
         configurationController.addCallback(new ConfigurationController.ConfigurationListener() {
             @Override
             public void onThemeChanged() {
@@ -90,13 +78,7 @@ public class ThemeOverlayControllerGoogle extends ThemeOverlayController {
             Log.d("ThemeOverlayController", "Boot animation colors " + i + ": " + i2);
         }
     }
-    
-    @Override
-    public void start() {
-        super.start();
-        darkConfigurationController.addCallback(darkConfigurationListener);
-    }
-        
+
     public final void setBootColorSystemProps() {
         try {
             int[] bootColors = getBootColors();
